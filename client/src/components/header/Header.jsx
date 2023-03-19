957ba209d4f84998650c197d62980134a0ca5730
Chirag <chirag.sharmaa36@gmail.com>
2023-03-19 09:58:48 +0530

    basic frontend 0.1

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react';
 
-import { AppBar, Toolbar, Box, Typography, IconButton, Drawer, List, styled } from '@mui/material';
-import { Menu } from '@mui/icons-material';
+import { AppBar, Toolbar, Box, Typography, styled } from '@mui/material';
 
 
 //components
@@ -13,6 +11,10 @@ const StyledHeader = styled(AppBar)`
     height: 55px;
 `;
 
+const Component = styled(Box)`
+margin-left:12%;
+line-height:0;
+`
 
 const SubHeading = styled(Typography)`
     font-size: 10px;
@@ -34,33 +36,24 @@ const CustomButtonWrapper = styled('span')(({ theme }) => ({
     }
 }));
 
+
 const Header = () => {
     const logoURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
     const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png';
-
-
-
-
-
-
     return (
         <StyledHeader position="fixed">
             <Toolbar style={{ minHeight: 55 }}>
-
-
-
-
-
-                <img src={logoURL} style={{ width: 75 }} />
-                <Box component="span" style={{ display: 'flex' }}>
-                    <SubHeading>Explore&nbsp;
-                        <Box component="span" style={{ color: '#FFE500' }}>
-                            Plus
-                        </Box>
-                    </SubHeading>
-                    <PlusImage src={subURL} />
-                </Box>
-
+                <Component>
+                    <img src={logoURL} style={{ width: 75 }} alt="logo"/>
+                    <Box component="span" style={{ display: 'flex' }}>
+                        <SubHeading>Explore&nbsp;
+                            <Box component="span" style={{ color: '#FFE500' }}>
+                                Plus
+                            </Box>
+                        </SubHeading>
+                        <PlusImage src={subURL} />
+                    </Box>
+                </Component>
                 <Search />
                 <CustomButtonWrapper>
                     <CustomButtons />
